69b2fbc6d16e381971a505bc627b98e336956c0a
AbhijeetHanwate <123650663+AbhijeetHanwate@users.noreply.github.com>
2023-01-30 23:07:11 +0530

    Task QA Lead

---
+++ HanwateProject/src/hanwatePackage/HanwateClass.java
@@ -0,0 +1,12 @@
+package hanwatePackage;
+
+public class HanwateClass {
+public static void main(String[] args) {
+	System.out.println("src/main/java");
+	System.out.println("src/main/test");
+	System.out.println("Test Layer");
+	System.out.println("Page Layer");
+	System.out.println("Test Base");
+	System.out.println("Utilities");
+}
+}
